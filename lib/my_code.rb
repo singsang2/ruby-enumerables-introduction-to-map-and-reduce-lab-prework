a3d27258a29026f92c26cf30ebe4bf65eda45233
Sung Bae <singsang2@gmail.com>
2020-04-08 19:15:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,9 @@
 # My Code here....
 def map_to_negativize(source_array)
-  source_array.each do |x|
-    
+  source_array.each_with_index do |x, i|
+    source_array[i] = -x
+  end
+  source_array
 end
 
 def map_to_no_change(source_array)
