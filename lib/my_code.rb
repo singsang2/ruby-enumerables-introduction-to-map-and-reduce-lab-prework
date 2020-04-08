9589cf858b37b9a836f94d0445ba6604b5b221c5
Sung Bae <singsang2@gmail.com>
2020-04-08 19:15:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,17 @@
 # My Code here....
 def map_to_negativize(source_array)
+  source_array.each do |x|
+    
+end
+
+def map_to_no_change(source_array)
+  
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
   
 end
